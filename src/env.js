e0fe1c38d896e07d0cfe879ca0e644bcdf3fa9e7
Xiaowen.Z <xiaowen.z@outlook.com>
2023-10-13 14:30:19 +0800

    fix: call zhipu

--- src/env.js
+++ src/env.js
@@ -1,4 +1,42 @@
+const ENV_VALUE_TYPE = {
+    ZHIPU_TOKEN: "string",
+    ZHIPU_MODEL: "string",
+};
+
+export const ENV = {
+    ZHIPU_TOKEN: null,
+    ZHIPU_MODEL: null,
+};
 
 export function initEnv(env) {
 
+    for (const key in ENV) {
+        if (env[key]) {
+            switch (ENV_VALUE_TYPE[key] || typeof ENV[key]) {
+                case "number":
+                    ENV[key] = parseInt(env[key]) || ENV[key];
+                    break;
+                case "boolean":
+                    ENV[key] = (env[key] || "false") === "true";
+                    break;
+                case "string":
+                    ENV[key] = env[key];
+                    break;
+                case "object":
+                    if (Array.isArray(ENV[key])) {
+                        ENV[key] = env[key].split(",");
+                    } else {
+                        try {
+                            ENV[key] = JSON.parse(env[key]);
+                        } catch (e) {
+                            console.error(e);
+                        }
+                    }
+                    break;
+                default:
+                    ENV[key] = env[key];
+                    break;
+            }
+        }
+    }
 }
--- src/zhipu.js
+++ src/zhipu.js
@@ -1,17 +1,99 @@
+import { ENV } from "./env";
+import * as jose from "jose";
 
 export async function completion(sentence) {
+    // init authentication information
+    let [id, secret] = ENV.ZHIPU_TOKEN.split(".");
+    if (id === undefined || secret === undefined) {
+        throw new Error("invalid apikey");
+    }
+
+    let timestamp = new Date().getTime();
+    let payload = {
+        api_key: id,
+        exp: timestamp + 1000 * 60,
+        timestamp: timestamp,
+    };
+    secret = new TextEncoder().encode(secret);
+    let jwt = new jose.SignJWT(payload).setProtectedHeader({
+        alg: "HS256",
+        sign_type: "SIGN",
+    });
+    let token = await jwt.sign(secret);
+
+    const headers = {
+        "Content-Type": "application/json",
+        accept: "text/event-stream",
+        Authorization: token,
+    };
+
+    // one time prompt
     const messages = [
-        { role: "system", content: "You are a friendly assistant" },
         {
             role: "user",
             content: sentence,
         },
     ];
 
-    
-    const response = await ai.run("@cf/meta/llama-2-7b-chat-int8", {
-        messages,
-    });
+    // json body
+    let body = {
+        prompt: messages,
+    };
 
-    return response["response"];
+    console.log(`Sending...\n${JSON.stringify(body)}`);
+    const res = await fetch(
+        `https://open.bigmodel.cn/api/paas/v3/model-api/${ENV.ZHIPU_MODEL}/sse-invoke`,
+        {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(body),
+        }
+    );
+
+    // read response from sse-invoke
+    if (res.ok) {
+        let target = "";
+        let errRes = res.clone();
+        const reader = res.body.getReader();
+        try {
+            while (true) {
+                const { done, value } = await reader.read();
+                if (done) {
+                    if (target === "") {
+                        let json = await errRes.json();
+                        if (json.msg) {
+                            throw json.msg;
+                        } else {
+                            throw JSON.stringify(json);
+                        }
+                    }
+                    return target.trim();
+                }
+                const str = new TextDecoder().decode(value);
+                console.log(`${str}`);
+                let list = str.split("\n");
+                for (let line of list) {
+                    if (line.startsWith("data:")) {
+                        let data = line.replace("data:", "");
+                        if (data === "") {
+                            target += "\n";
+                        } else {
+                            target += data;
+                        }
+                    }
+                }
+            }
+        } finally {
+            reader.releaseLock();
+            try {
+                errRes.json();
+            } catch (e) {
+                console.log(`Finally clean requestClone failed...never mind.`)
+            }
+        }
+    } else {
+        throw `Http Request Error\nHttp Status: ${res.status}\n${JSON.stringify(
+            res.data
+        )}`;
+    }
 }
